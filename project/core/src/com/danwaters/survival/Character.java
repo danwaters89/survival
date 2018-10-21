54eaea0514e6f8bc02e53334f0e94f8f6c06b21b
danwaters89 <daniel.leach.waters@gmail.com>
2018-10-21 14:20:35 -0700

    Switch sprites when turning (#3)

--- project/core/src/com/danwaters/survival/Character.java
+++ project/core/src/com/danwaters/survival/Character.java
@@ -4,34 +4,80 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Disposable;
 
 public class Character {
 
-    private final Sprite sprite;
+    private Sprite currentSprite;
+    private Direction direction;
+
+    private final Sprite leftSprite;
+    private final Sprite rightSprite;
+    private final Sprite upSprite;
+    private final Sprite downSprite;
 
     public Character() {
         Texture texture = new Texture("core/assets/Sprites.png");
-        TextureRegion region = new TextureRegion(texture, 32, 32);
-        this.sprite = new Sprite(region);
+        TextureRegion region = new TextureRegion(texture, 2, 2, 32, 32);
+        this.downSprite = new Sprite(region);
+        region = new TextureRegion(texture, 2, 36, 32, 32);
+        this.leftSprite = new Sprite(region);
+        this.rightSprite = new Sprite(region);
+        rightSprite.flip(true, false);
+        region = new TextureRegion(texture, 2, 70, 32, 32);
+        this.upSprite = new Sprite(region);
+        this.currentSprite = downSprite;
+        direction = Direction.DOWN;
     }
 
     public void draw(SpriteBatch batch) {
-        sprite.draw(batch);
+        currentSprite.draw(batch);
     }
 
     public void moveLeft() {
-        sprite.translateX(-32);
+        if (direction == Direction.LEFT) {
+            currentSprite.translateX(-32);
+        } else {
+            direction = Direction.LEFT;
+            leftSprite.setPosition(currentSprite.getX(), currentSprite.getY());
+            currentSprite = leftSprite;
+        }
     }
 
     public void moveRight() {
-        sprite.translateX(32);
+        if (direction == Direction.RIGHT) {
+            currentSprite.translateX(32);
+        } else {
+            direction = Direction.RIGHT;
+            rightSprite.setPosition(currentSprite.getX(), currentSprite.getY());
+            currentSprite = rightSprite;
+        }
     }
 
     public void moveUp() {
-        sprite.translateY(32);
+        if (direction == Direction.UP) {
+            currentSprite.translateY(32);
+        } else {
+            direction = Direction.UP;
+            upSprite.setPosition(currentSprite.getX(), currentSprite.getY());
+            currentSprite = upSprite;
+        }
     }
 
     public void moveDown() {
-        sprite.translateY(-32);
+        if (direction == Direction.DOWN) {
+            currentSprite.translateY(-32);
+        } else {
+            direction = Direction.DOWN;
+            downSprite.setPosition(currentSprite.getX(), currentSprite.getY());
+            currentSprite = downSprite;
+        }
+    }
+
+    private enum Direction {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
     }
 }
