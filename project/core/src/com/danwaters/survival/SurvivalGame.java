6f3103d9f86255fb6cfea970ed4d04e316b340d7
Daniel Waters <danielwaters@Daniels-MacBook-Pro.local>
2018-10-23 21:15:34 -0700

    Transparent plants

--- project/core/src/com/danwaters/survival/SurvivalGame.java
+++ project/core/src/com/danwaters/survival/SurvivalGame.java
@@ -63,7 +63,7 @@ public class SurvivalGame extends ApplicationAdapter {
         if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
             map.getLayers().get(1).setVisible(!map.getLayers().get(1).isVisible());
         }
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.3f, 0.5f, 0.5f, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
